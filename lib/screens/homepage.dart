d064513353433cbd33b2a9c3c893eabeeed64e79
CocheteuxLaurent <laurent.cocheteux@hotmail.fr>
2023-10-11 12:02:31 +0200

    update new information display assets file creation

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -4,9 +4,9 @@ import 'package:flutter/material.dart';
 import '../api/api_service.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({
+    super.key,
+  });
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -24,20 +24,51 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Weather App'),
       ),
       body: Column(
         children: [
-          Text(widget.title),
           FutureBuilder(
             future: weatherFunction,
             builder: (context, snap) {
               print(snap.data?.id); //test d'affiche des données
-              var id = snap.data?.id;
+              //var id = snap.data?.id;
+              var main = snap.data?.main;
+              var icon = snap.data?.icon;
               var description = snap.data?.description;
-              return Container(
+              return /*
+              Container(
                 child: Text(
-                  description.toString(),
+                  main.toString(),
+                ),
+              );
+              */
+                  Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          main.toString(),
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            description.toString(),
+                            style: const TextStyle(fontSize: 12),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               );
             },
