967daa944c2e6cfb866fcf8758c8e93e188d5553
SamirBenj <sciencecalc7@gmail.com>
2023-10-13 09:03:52 +0200

    resolved weather card overflow component

--- lib/components/weather_card.dart
+++ lib/components/weather_card.dart
@@ -19,37 +19,68 @@ class _WeatherCardState extends State<WeatherCard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          FutureBuilder(
-            future: weatherFunction,
-            builder: (context, snap) {
-              print(snap.data?.id); //test d'affiche des données
-              //var id = snap.data?.id;
-              var main = snap.data?.main;
-              var icon = snap.data?.icon ??
-                  "loading"; //image loading ajoutée temporairement
-              var description = snap.data?.description;
-              var temp = snap.data?.temp;
-              var humidity = snap.data?.humidity;
-              return Card(
-                color: Color.fromRGBO(239, 241, 243, 1),
-                child: SizedBox(
-                  height: 100,
-                  width: 300,
-                  child: Column(
+      body: FutureBuilder(
+        future: weatherFunction,
+        builder: (context, snap) {
+          print(snap.data?.id); //test d'affiche des données
+          //var id = snap.data?.id;
+          var main = snap.data?.main;
+          var icon = snap.data?.icon ??
+              "loading"; //image loading ajoutée temporairement
+          var description = snap.data?.description;
+          var temp = snap.data?.temp;
+          var humidity = snap.data?.humidity;
+          return Card(
+            color: Color.fromRGBO(239, 241, 243, 1),
+            child: SizedBox(
+              height: 100,
+              width: 300,
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Image.asset(
+                      "assets/$icon.png",
+                      scale: 4,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Image.asset(
-                        "assets/$icon.png",
-                        scale: 4,
+                      Text(
+                        main.toString(),
+                        style: const TextStyle(fontSize: 16),
                       ),
                     ],
                   ),
-                ),
-              );
-            },
-          )
-        ],
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          description.toString(),
+                          style: const TextStyle(fontSize: 12),
+                        ),
+                        Text(
+                          temp.toString(),
+                          style: const TextStyle(fontSize: 12),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        humidity.toString(),
+                        style: const TextStyle(fontSize: 12),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -30,73 +30,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Column(
         children: [
-          WeatherCard(),
-          /*
-          FutureBuilder(
-            future: weatherFunction,
-            builder: (context, snap) {
-              print(snap.data?.id); //test d'affiche des données
-              //var id = snap.data?.id;
-              var main = snap.data?.main;
-              var icon = snap.data?.icon ??
-                  "loading"; //image loading ajoutée temporairement
-              var description = snap.data?.description;
-              var temp = snap.data?.temp;
-              var humidity = snap.data?.humidity;
-              return /*
-              Container(
-                child: Text(
-                  main.toString(),
-                ),
-              );
-              */
-                  Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: Column(
-                  children: [
-                    Image.asset(
-                      "assets/$icon.png",
-                      scale: 4,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          main.toString(),
-                          style: const TextStyle(fontSize: 16),
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 8),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            description.toString(),
-                            style: const TextStyle(fontSize: 12),
-                          ),
-                          Text(
-                            temp.toString(),
-                            style: const TextStyle(fontSize: 12),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          humidity.toString(),
-                          style: const TextStyle(fontSize: 12),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              );
-            },
-          ),
-          */
+          Expanded(child: WeatherCard()),
           Expanded(
             child: BottomBar(),
           ),
