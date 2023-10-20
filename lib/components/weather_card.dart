95be5605d896174ace9260bc5aa77189a20bcc5a
CocheteuxLaurent <laurent.cocheteux@hotmail.fr>
2023-10-20 10:16:36 +0200

    new update

--- lib/components/weather_card.dart
+++ lib/components/weather_card.dart
@@ -29,110 +29,87 @@ class _WeatherCardState extends State<WeatherCard> {
         var description = snap.data?.description;
         var temp = snap.data?.temp;
         var humidity = snap.data?.humidity;
-        return Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
-          ),
-          color: const Color.fromRGBO(239, 241, 243, 1),
-          child: SizedBox(
-            width: 300,
-            height: 50,
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              color: const Color.fromRGBO(239, 241, 243, 1),
+              child: SizedBox(
+                width: 300,
+                height: 80,
+                child: Column(
                   children: [
-                    Image.asset(
-                      "assets/$icon.png",
-                      scale: 4,
-                      height: 50,
-                      width: 50,
-                    ),
-                    Text(
-                      main.toString(),
-                      style: const TextStyle(fontSize: 13),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 8.0),
-                      child: Text(
-                        temp.toString(),
-                        style: const TextStyle(fontSize: 13),
-                      ),
-                    ),
-                  ],
-                )
-                /*
-                  Row(
-                    children: [
-                      Image.asset(
-                        "assets/$icon.png",
-                        scale: 4,
-                        height: 50,
-                        width: 50,
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        main.toString(),
-                        style: const TextStyle(fontSize: 13),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 6, left: 6),
-                        child: Text(
-                          temp.toString(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Image.asset(
+                          "assets/$icon.png",
+                          scale: 4,
+                          height: 50,
+                          width: 50,
+                        ),
+                        Text(
+                          main.toString(),
                           style: const TextStyle(fontSize: 13),
                         ),
-                      ),
-                    ],
-                  ),
-                */
-                /*  
-                  Expanded(
-                    child: Image.asset(
-                      "assets/$icon.png",
-                      scale: 4,
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        main.toString(),
-                        style: const TextStyle(fontSize: 16),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 8),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          description.toString(),
-                          style: const TextStyle(fontSize: 12),
-                        ),
-                        Text(
-                          temp.toString(),
-                          style: const TextStyle(fontSize: 12),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: Text(
+                            temp.toString(),
+                            style: const TextStyle(fontSize: 13),
+                          ),
                         ),
                       ],
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        humidity.toString(),
-                        style: const TextStyle(fontSize: 12),
+                    )
+                    /*  
+                      Expanded(
+                        child: Image.asset(
+                          "assets/$icon.png",
+                          scale: 4,
+                        ),
                       ),
-                    ],
-                  ),
-                  */
-              ],
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            main.toString(),
+                            style: const TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              description.toString(),
+                              style: const TextStyle(fontSize: 12),
+                            ),
+                            Text(
+                              temp.toString(),
+                              style: const TextStyle(fontSize: 12),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            humidity.toString(),
+                            style: const TextStyle(fontSize: 12),
+                          ),
+                        ],
+                      ),
+                      */
+                  ],
+                ),
+              ),
             ),
-          ),
+          ],
         );
       },
     );
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -2,6 +2,7 @@ import 'package:appweather/class/classWeather.dart';
 import 'package:flutter/material.dart';
 import 'package:appweather/components/bottom_bar.dart';
 import 'package:appweather/components/weather_card.dart';
+import 'package:geolocator/geolocator.dart ';
 
 import '../api/api_service.dart';
 
@@ -17,9 +18,55 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   //instance de la class
   var weatherFunction = WeatherFunction.getWeather("Valenciennes");
+
+  Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+
+
+  Position? _currentPosition;
+
   @override
   void initState() {
     super.initState();
+  }
+
+  Future<bool> _handleLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location services are disabled. Please enable the services')));
+      return false;
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Location permissions are denied')));
+        return false;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location permissions are permanently denied, we cannot request permissions.')));
+      return false;
+    }
+    return true;
+  }
+
+  Future<void> _getCurrentPosition() async {
+    final hasPermission = await _handleLocationPermission();
+    if (!hasPermission) return;
+    await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+        .then((Position position) {
+      setState(() => _currentPosition = position);
+    }).catchError((e) {
+      debugPrint(e);
+    });
   }
 
   @override
